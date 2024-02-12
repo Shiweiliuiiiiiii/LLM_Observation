e0ce03ca099f990b0737b41eda5a6115cf25429b
刘世伟 <liushiwei@v099250.vpn.tue.nl>
2024-02-12 21:33:14 +0000

    measure norm

--- plots/norm_visualization.py
+++ plots/norm_visualization.py
@@ -1,6 +1,7 @@
 import torch
 
+a_norm = torch.load('../opt_6.7b_wiki103_emb-a-norm.pt', map_location=torch.device('cpu'), weights_only=False)
+v_norm = torch.load('../opt_6.7b_wiki103_emb-v-norm.pt', map_location=torch.device('cpu'), weights_only=False)
 
-a_norm = torch.load('../opt_6.7b_wiki103_emb-a-norm.pt', map_location=torch.device('cpu'), weights_only=False
-                    )
-print(len(a_norm))
+
+print(len(a_norm[0]))
--- scripts/obs/emb_opt.sh
+++ scripts/obs/emb_opt.sh
@@ -1,4 +1,17 @@
-GPU=3
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --gpus=1
+#SBATCH -t 1-00:00:00
+#SBATCH --cpus-per-task=18
+#SBATCH -o opt_norm_v_a.out
+
+source /home/sliu01/anaconda3/etc/profile.d/conda.sh
+source activate UW
+
+GPU=0
 CUDA_VISIBLE_DEVICES=${GPU} python -u main_observation_emb_opt.py \
     --dataset_name wikitext \
     --dataset_config_name wikitext-103-raw-v1 \
@@ -6,4 +19,4 @@ CUDA_VISIBLE_DEVICES=${GPU} python -u main_observation_emb_opt.py \
     --output_dir tmp \
     --per_device_eval_batch_size 1 \
     --per_device_train_batch_size 1 \
-    --output_name opt_6.7b_wiki103_emb
+    --output_name opt_6.7b_wiki103_att
